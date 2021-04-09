856722cdbd1e5a27660b680f289dadfa5f1af5fa
David Racero <canillas.mail@gmail.com>
2021-04-09 18:26:48 +0200

    Added reserve lending pool tests and claim tests.

--- test-fork/incentivesProposal.spec.ts
+++ test-fork/incentivesProposal.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import rawHRE from 'hardhat';
 import { BigNumber } from 'ethers';
-import { formatEther, parseEther } from 'ethers/lib/utils';
+import { formatEther, parseEther, parseUnits } from 'ethers/lib/utils';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 import { JsonRpcSigner } from '@ethersproject/providers';
 
@@ -20,16 +20,22 @@ import { IAaveGovernanceV2 } from '../types/IAaveGovernanceV2';
 import { ILendingPool } from '../types/ILendingPool';
 import {
   AaveIncentivesControllerFactory,
+  AaveProtocolDataProviderFactory,
   AToken,
+  ATokenFactory,
   InitializableAdminUpgradeabilityProxyFactory,
   ProposalIncentivesExecutorFactory,
   SelfdestructTransferFactory,
+  VariableDebtTokenFactory,
 } from '../types';
 import { parse } from 'dotenv/types';
 import { tEthereumAddress } from '../helpers/types';
+import { ILendingPoolAddressesProviderFactory } from '../types/ILendingPoolAddressesProviderFactory';
+import { IERC20Factory } from '../types/IERC20Factory';
+import { IATokenFactory } from '../types/IATokenFactory';
 
 const {
-  RESERVES = 'USDT,USDC,DAI,WETH,WBTC,GUSD',
+  RESERVES = 'DAI,GUSD,USDC,USDT,WBTC,WETH',
   POOL_CONFIGURATOR = '0x311bb771e4f8952e6da169b425e7e92d6ac45756',
   POOL_PROVIDER = '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
   POOL_DATA_PROVIDER = '0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d',
@@ -64,6 +70,45 @@ const AAVE_STAKE = '0x4da27a545c0c5B758a6BA100e3a049001de870f5';
 const DAI_TOKEN = '0x6b175474e89094c44da98b954eedeac495271d0f';
 const DAI_HOLDER = '0x72aabd13090af25dbb804f84de6280c697ed1150';
 
+const spendList = {
+  DAI: {
+    holder: '0x72aabd13090af25dbb804f84de6280c697ed1150',
+    transfer: '1000',
+    deposit: '100',
+    decimals: '18',
+  },
+  GUSD: {
+    holder: '0x3e6722f32cbe5b3c7bd3dca7017c7ffe1b9e5a2a',
+    transfer: '1000',
+    deposit: '100',
+    decimals: '2',
+  },
+  USDC: {
+    holder: '0xAe2D4617c862309A3d75A0fFB358c7a5009c673F',
+    transfer: '1000',
+    deposit: '100',
+    decimals: '6',
+  },
+  USDT: {
+    holder: '0x9f57dc21f521c64204b6190c3076a05b559b1a28',
+    transfer: '1000',
+    deposit: '100',
+    decimals: '6',
+  },
+  WBTC: {
+    holder: '0x6dab3bcbfb336b29d06b9c793aef7eaa57888922',
+    transfer: '1',
+    deposit: '0.5',
+    decimals: '8',
+  },
+  WETH: {
+    holder: '0x0f4ee9631f4be0a63756515141281a3e2b293bbe',
+    transfer: '1',
+    deposit: '0.5',
+    decimals: '18',
+  },
+};
+
 describe('Enable incentives in target assets', () => {
   let ethers;
 
@@ -75,6 +120,7 @@ describe('Enable incentives in target assets', () => {
   let gov: IAaveGovernanceV2;
   let pool: ILendingPool;
   let aave: IERC20;
+  let stkAave: IERC20;
   let dai: IERC20;
   let aDAI: AToken;
   let variableDebtDAI: IERC20;
@@ -153,7 +199,10 @@ describe('Enable incentives in target assets', () => {
         value: ethers.utils.parseEther('1'),
       })
     ).wait();
-    await impersonateAccountsHardhat([AAVE_WHALE, DAI_HOLDER]);
+    await impersonateAccountsHardhat([
+      AAVE_WHALE,
+      ...Object.keys(spendList).map((k) => spendList[k].holder),
+    ]);
 
     // Impersonating holders
     whale = ethers.provider.getSigner(AAVE_WHALE);
@@ -177,22 +226,11 @@ describe('Enable incentives in target assets', () => {
       variableDebtTokenAddress,
     } = await pool.getReserveData(DAI_TOKEN);
 
-    aave = (await ethers.getContractAt(
-      '@aave/aave-stake/contracts/interfaces/IERC20.sol:IERC20',
-      AAVE_TOKEN,
-      whale
-    )) as IERC20;
-    dai = (await ethers.getContractAt(
-      '@aave/aave-stake/contracts/interfaces/IERC20.sol:IERC20',
-      DAI_TOKEN,
-      daiHolder
-    )) as IERC20;
-    aDAI = (await ethers.getContractAt('AToken', aTokenAddress, proposer)) as AToken;
-    variableDebtDAI = (await ethers.getContractAt(
-      '@aave/aave-stake/contracts/interfaces/IERC20.sol:IERC20',
-      variableDebtTokenAddress,
-      proposer
-    )) as IERC20;
+    aave = IERC20Factory.connect(AAVE_TOKEN, whale);
+    stkAave = IERC20Factory.connect(AAVE_STAKE, proposer);
+    dai = IERC20Factory.connect(DAI_TOKEN, daiHolder);
+    aDAI = ATokenFactory.connect(aTokenAddress, proposer);
+    variableDebtDAI = IERC20Factory.connect(variableDebtTokenAddress, proposer);
 
     // Transfer enough AAVE to proposer
     await (await aave.transfer(proposer.address, parseEther('1000000'))).wait();
@@ -279,17 +317,150 @@ describe('Enable incentives in target assets', () => {
     expect(await aDAI.balanceOf(proposer.address)).to.be.eq('0');
     expect(afterDAIBalance).to.be.gt(priorDAIBalance);
   });
-  xit('User should be use LendingPool with GUSD/USDC/USDT/WBTC/WETH');
-  xit('Check all aToken symbols matches', () => {});
-  xit('Check all variable debt symbols matches', () => {});
-  xit('Users should be able to claim incentives', async () => {
-    const {
-      configuration: { data },
-      aTokenAddress,
-      stableDebtTokenAddress,
-      variableDebtTokenAddress,
-    } = await pool.getReserveData(DAI_TOKEN);
+  it('User should be able to interact with LendingPool with DAI/GUSD/USDC/USDT/WBTC/WETH', async () => {
+    const poolProvider = await ILendingPoolAddressesProviderFactory.connect(
+      POOL_PROVIDER,
+      proposer
+    );
+    const protocolDataProvider = await AaveProtocolDataProviderFactory.connect(
+      await poolProvider.getAddress(
+        '0x0100000000000000000000000000000000000000000000000000000000000000'
+      ),
+      proposer
+    );
 
-    // Claim rewards TBD
+    const reserveConfigs = (await protocolDataProvider.getAllReservesTokens())
+      .filter(({ symbol }) => RESERVES.includes(symbol))
+      .sort(({ symbol: a }, { symbol: b }) => a.localeCompare(b));
+
+    // Deposit AAVE to LendingPool to have enought collateral for future borrows
+    await (await aave.connect(proposer).approve(pool.address, parseEther('1000'))).wait();
+    await (
+      await pool.connect(proposer).deposit(aave.address, parseEther('1000'), proposer.address, 0)
+    ).wait();
+
+    for (let x = 0; x < reserveConfigs.length; x++) {
+      const { aTokenAddress, variableDebtTokenAddress } = await pool.getReserveData(DAI_TOKEN);
+      const { symbol, tokenAddress } = reserveConfigs[x];
+      const reserve = IERC20Factory.connect(tokenAddress, proposer);
+      const aToken = ATokenFactory.connect(aTokenAddress, proposer);
+      const holderSigner = ethers.provider.getSigner(spendList[symbol].holder);
+
+      // Transfer assets to proposer from reserve holder
+      await (
+        await reserve
+          .connect(holderSigner)
+          .transfer(
+            proposer.address,
+            parseUnits(spendList[symbol].transfer, spendList[symbol].decimals)
+          )
+      ).wait();
+
+      // Amounts
+      const depositAmount = parseUnits(spendList[symbol].deposit, spendList[symbol].decimals);
+      const borrowAmount = depositAmount.div('10');
+
+      // Deposit to LendingPool
+      await (await reserve.connect(proposer).approve(pool.address, depositAmount)).wait();
+      const tx1 = await pool
+        .connect(proposer)
+        .deposit(reserve.address, depositAmount, proposer.address, 0);
+      await tx1.wait();
+      expect(tx1).to.emit(pool, 'Deposit');
+
+      // Request loan to LendingPool
+      const tx2 = await pool.borrow(reserve.address, borrowAmount, '2', '0', proposer.address);
+      await tx2.wait();
+      expect(tx2).to.emit(pool, 'Borrow');
+
+      // Repay variable loan to LendingPool
+      await (await reserve.connect(proposer).approve(pool.address, MAX_UINT_AMOUNT)).wait();
+      const tx3 = await pool.repay(reserve.address, MAX_UINT_AMOUNT, '2', proposer.address);
+      await tx3.wait();
+      expect(tx3).to.emit(pool, 'Repay');
+
+      // Withdraw from LendingPool
+      const priorBalance = await reserve.balanceOf(proposer.address);
+      await (await aToken.connect(proposer).approve(pool.address, MAX_UINT_AMOUNT)).wait();
+      const tx4 = await pool.withdraw(reserve.address, MAX_UINT_AMOUNT, proposer.address);
+      await tx4.wait();
+      expect(tx4).to.emit(pool, 'Withdraw');
+
+      const afterBalance = await reserve.balanceOf(proposer.address);
+      expect(await aToken.balanceOf(proposer.address)).to.be.eq('0');
+      expect(afterBalance).to.be.gt(priorBalance);
+    }
+  });
+  xit('Check all aToken symbols and debt token matches', async () => {
+    const poolProvider = await ILendingPoolAddressesProviderFactory.connect(
+      POOL_PROVIDER,
+      proposer
+    );
+    const protocolDataProvider = await AaveProtocolDataProviderFactory.connect(
+      await poolProvider.getAddress(
+        '0x0100000000000000000000000000000000000000000000000000000000000000'
+      ),
+      proposer
+    );
+
+    const reserveConfigs = (await protocolDataProvider.getAllReservesTokens())
+      .filter(({ symbol }) => RESERVES.includes(symbol))
+      .sort(({ symbol: a }, { symbol: b }) => a.localeCompare(b));
+
+    console.log('prior');
+    for (let x = 0; x < reserveConfigs.length; x++) {
+      const { aTokenAddress, variableDebtTokenAddress } = await pool.getReserveData(DAI_TOKEN);
+      const { symbol, tokenAddress } = reserveConfigs[x];
+      const reserve = IERC20Factory.connect(tokenAddress, proposer);
+      const aToken = ATokenFactory.connect(aTokenAddress, proposer);
+      const varDebtToken = VariableDebtTokenFactory.connect(variableDebtTokenAddress, proposer);
+    }
+  });
+  it('Users should be able to claim incentives', async () => {
+    // Initialize proxy for incentives controller
+    const incentives = AaveIncentivesControllerFactory.connect(incentivesProxy, proposer);
+    const poolProvider = await ILendingPoolAddressesProviderFactory.connect(
+      POOL_PROVIDER,
+      proposer
+    );
+    const protocolDataProvider = await AaveProtocolDataProviderFactory.connect(
+      await poolProvider.getAddress(
+        '0x0100000000000000000000000000000000000000000000000000000000000000'
+      ),
+      proposer
+    );
+
+    const reserveConfigs = (await protocolDataProvider.getAllReservesTokens())
+      .filter(({ symbol }) => RESERVES.includes(symbol))
+      .sort(({ symbol: a }, { symbol: b }) => a.localeCompare(b));
+
+    for (let x = 0; x < reserveConfigs.length; x++) {
+      const { tokenAddress, symbol } = reserveConfigs[x];
+      const { aTokenAddress, variableDebtTokenAddress } = await pool.getReserveData(
+        reserveConfigs[x].tokenAddress
+      );
+      const reserve = IERC20Factory.connect(tokenAddress, proposer);
+
+      // Amounts
+      const depositAmount = parseUnits(spendList[symbol].deposit, spendList[symbol].decimals);
+
+      // Deposit to LendingPool
+      await (await reserve.connect(proposer).approve(pool.address, '0')).wait();
+      await (await reserve.connect(proposer).approve(pool.address, depositAmount)).wait();
+      await (
+        await pool.connect(proposer).deposit(reserve.address, depositAmount, proposer.address, 0)
+      ).wait();
+      await increaseTime(1296000);
+
+      const priorBalance = await stkAave.balanceOf(proposer.address);
+      const tx = await incentives
+        .connect(proposer)
+        .claimRewards([aTokenAddress, variableDebtTokenAddress], MAX_UINT_AMOUNT, proposer.address);
+      await tx.wait();
+      expect(tx).to.emit(incentives, 'RewardsClaimed');
+
+      const afterBalance = await stkAave.balanceOf(proposer.address);
+      expect(afterBalance).to.be.gt(priorBalance);
+    }
   });
 });
