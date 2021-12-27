62775bc77e1b62507355b5988467d02f729ce106
dongcool <95206510+dongcool@users.noreply.github.com>
2021-12-27 01:56:07 +0000

    aurora config

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -35,8 +35,8 @@ export const NETWORKS_RPC_URL: iParamsPerNetwork<string> = {
   [eXDaiNetwork.xdai]: 'https://rpc.xdaichain.com/',
   [eAvalancheNetwork.avalanche]: 'https://cchain.explorer.avax.network/',
   [eAvalancheNetwork.fuji]: 'https://api.avax-test.network/ext/bc/C/rpc',
-  // [eAuroraNetwork.aurora]: 'https://mainnet.aurora.dev',
-  [eAuroraNetwork.aurora]: 'http://localhost:8545',
+  [eAuroraNetwork.aurora]: 'https://mainnet.aurora.dev/5eZVevhGw6jT8N8nuXZF14NzfWPibWozE2oNa6Phpbio',
+  // [eAuroraNetwork.aurora]: 'http://localhost:8545',
 };
 
 export const NETWORKS_DEFAULT_GAS: iParamsPerNetwork<number> = {
@@ -50,6 +50,7 @@ export const NETWORKS_DEFAULT_GAS: iParamsPerNetwork<number> = {
   [ePolygonNetwork.mumbai]: 1 * GWEI,
   [ePolygonNetwork.matic]: 2 * GWEI,
   [eXDaiNetwork.xdai]: 1 * GWEI,
+  [eAuroraNetwork.aurora]: 0 * GWEI,
   [eAvalancheNetwork.fuji]: 225 * GWEI,
   [eAvalancheNetwork.avalanche]: 225 * GWEI,
 };
