8bb0b5693daf1288fc5b078933e5159ebae6618c
Sean Casey <sean@seanjcasey.com>
2022-04-21 08:18:15 -0400

    refactor: fix variable name

--- contracts/proposals/ProposalIncentivesRev2Executor.sol
+++ contracts/proposals/ProposalIncentivesRev2Executor.sol
@@ -5,13 +5,13 @@ import {ILendingPoolAddressesProvider} from '../interfaces/ILendingPoolAddresses
 import {IProposalIncentivesRev2Executor} from '../interfaces/IProposalIncentivesRev2Executor.sol';
 
 contract ProposalIncentivesRev2Executor is IProposalIncentivesRev2Executor {
+  bytes32 constant INCENTIVES_CONTROLLER_ID = bytes32(keccak256(bytes('INCENTIVES_CONTROLLER')));
   address constant INCENTIVES_CONTROLLER_IMPL_ADDRESS = 0xD9ED413bCF58c266F95fE6BA63B13cf79299CE31;
   address constant LENDING_POOL_ADDRESS_PROVIDER = 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5;
-  bytes32 constant LENDING_POOL_ID = bytes32(keccak256(bytes('INCENTIVES_CONTROLLER')));
 
   function execute() external override {
     ILendingPoolAddressesProvider(LENDING_POOL_ADDRESS_PROVIDER).setAddressAsProxy(
-      LENDING_POOL_ID,
+      INCENTIVES_CONTROLLER_ID,
       INCENTIVES_CONTROLLER_IMPL_ADDRESS
     );
   }
